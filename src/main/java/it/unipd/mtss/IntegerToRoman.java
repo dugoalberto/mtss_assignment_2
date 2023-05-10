cb71a2914ebe67d419e3b5b09de51768e159aa5e
Lorenzo <bencylorenzo@gmail.com>
2023-05-10 17:19:49 +0200

    close #4

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -5,7 +5,30 @@
 package it.unipd.mtss;
 
 public class IntegerToRoman {
-    public static String convert(int number){
-        return null;
+    private static final int[] VALUES = { 1000, 900, 500, 400, 100, 90, 50, 40,
+        10, 9, 5, 4, 1 };
+private static final String[] SYMBOLS = { "M", "CM", "D", "CD", "C", "XC",
+        "L", "XL", "X", "IX", "V", "IV", "I" };
+
+public static String convert(int number)
+        throws NumberUnderZeroException, NumberOverThousand {
+    if (number < 0) {
+        throw new NumberUnderZeroException();
     }
+    if (number > 1000) {
+        throw new NumberOverThousand();
+    }
+
+    StringBuilder roman = new StringBuilder();
+    int remaining = number;
+
+    for (int i = 0; i < VALUES.length; i++) {
+        while (remaining >= VALUES[i]) {
+            roman.append(SYMBOLS[i]);
+            remaining -= VALUES[i];
+        }
+    }
+
+    return roman.toString();
+}
 }
